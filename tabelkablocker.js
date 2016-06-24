fc9852cf01f521e07b82a1c369f2def38def15dc
Emil <emilszymecki@gmail.com>
2016-06-24 17:10:05 +0200

    Update tabelkablocker.js

--- tabelkablocker.js
+++ tabelkablocker.js
@@ -1,33 +1,35 @@
 function tableLimiter(name,n,lim,hv,block){
 
-$(".page-name-"+name+" .answer").on("change",function(e){
-    var make;
-    if(hv==0){
-    	 make = $(".page-name-"+name+" .answer").parents(".col-"+n);
-    }
-    if(hv==1){
-    	make = $(".page-name-"+name+" .answer").parents("tbody").find("tr").eq(n-1);
-    }
-    var limit = lim;
-    var active = make.find(".answer:checked");
-    var deactive = make.find(".answer:not(:checked)");
-    if(active.length >= limit){
-      deactive.attr('disabled',true);
-    }else{
-      deactive.attr('disabled',false);
-    }
-    
-});
+	$(".page-name-"+name+" .answer").on("change",function(e){
+	    var make;
+	    if(hv==0){
+	    	 make = $(".page-name-"+name+" .answer").parents(".col-"+n);
+	    }
+	    if(hv==1){
+	    	make = $(".page-name-"+name+" .answer").parents("tbody").find("tr").eq(n-1);
+	    }
+	    var limit = lim;
+	    var active = make.find(".answer:checked");
+	    var deactive = make.find(".answer:not(:checked)");
+	    if(active.length >= limit){
+	      deactive.attr('disabled',true);
+	    }else{
+	      deactive.attr('disabled',false);
+	    }
+	    
+	});
 
     $(".page-name-"+name+" .answer").parents(".col-"+block).find(".answer").on("change",function(x){
-    	 $(this).parents("tr").find(".answer").attr("disabled",false);
+    	var parentRow =  $(this).parents("tr");
+    	var nonClickTd = parentRow.find(".clickTd:not(.col-"+block+")");
+    	 parentRow.find(".answer").attr("disabled",false);
 	    if($(this).is(":checked")){
-	      $(this).parents("tr").find(".clickTd:not(.col-"+block+")").find(".answer").prop( "checked", false );
-	      $(this).parents("tr").find(".clickTd:not(.col-"+block+")").find(".answer").attr("disabled",true).hide();
-	      $(this).parents("tr").find(".clickTd:not(.col-"+block+")").append('<span class="glyphicon glyphicon-remove"></span>');
+	      nonClickTd.find(".answer").prop( "checked", false );
+	      nonClickTd.find(".answer").attr("disabled",true).hide();
+	      nonClickTd.append('<span class="glyphicon glyphicon-remove"></span>');
 	    }else{
-	    	$(this).parents("tr").find(".clickTd:not(.col-"+block+")").find(".glyphicon-remove").hide();
-	      $(this).parents("tr").find(".clickTd:not(.col-"+block+")").find(".answer").show();
+	    	nonClickTd.find(".glyphicon-remove").hide();
+	      nonClickTd.find(".answer").show();
 	    }
 		});
 
