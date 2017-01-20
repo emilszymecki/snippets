10ec3c76840cd17feca5180444ee04d9c17d0ea8
Emil <emilszymecki@gmail.com>
2017-01-20 14:45:35 +0100

    Update Beeline2017.r
    zmiana2

--- Beeline2017.r
+++ Beeline2017.r
@@ -13,3 +13,14 @@ for(i in 1:length(label)){
         y[1,nazwa_col] <- NA
     }
 }  #Tworzę nowego data.frame() do obsadzania danych 
+
+
+for(i in 1:nrow(Beeline)){  #Pętla po wierszach
+      rcat <- as.character(Beeline$Kat[i]) #zapisuje jako tekst wartość kategorii z Beeline$Kat
+    for(j in 1:length(x)){ # pętla po wszystkich nazwach kolumn
+        cname <- x[j] # wyciągam nazwę kolumny jako string z x'a
+        cval <- as.character(Beeline[i,cname]) # wartość jako charakter z konkretnego wiersza i kolumny
+        new_col_name <- paste(rcat,cname,sep="_"); # tworzę nazwę kolumny pasującej do ramki y typu garnki_II_...
+        y[i,new_col_name] <- cval #Do konkretnego wiersza i kolumny wpisuje wartość
+    }
+}
