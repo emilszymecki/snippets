2cb9c5b53b23c586823a0158f65b930ae16d7f89
Emil <emilszymecki@gmail.com>
2017-07-07 14:21:09 +0200

    rotowanie kolumn

---
+++ colShuffle.js
@@ -0,0 +1,46 @@
+function colShuffle(po){
+
+var arr = [];
+
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+function arraymove(el) {
+    var stack = arr.indexOf(el);
+    var elm = arr[stack];
+    arr.splice(stack, 1);
+    arr.splice(el-1, 0, elm);
+}
+
+
+$(po).find('table tr').each(function(){
+   $(this).children().each(function(a,b){
+   	 if(arr.indexOf(a) == -1 && a != 0){
+   	 	arr.push(a);
+   	 }
+     $(this).data('col',a);
+   });
+});
+
+shuffle(arr);
+
+
+
+
+console.log(arr);
+for(var i = 0; i<arr.length; i++){
+	$(':data("col")').filter(function(){
+ 	if($(this).data('col') == arr[i]){
+   		$(this).parent().append($(this));
+ 	}
+	});
+}
+
+}
