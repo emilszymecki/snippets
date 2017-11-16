78d07cb5edccb1c7b932562917655561521aba2a
Emil <emilszymecki@gmail.com>
2017-11-16 12:45:00 +0100

    Update validator.js

--- validator.js
+++ validator.js
@@ -94,3 +94,110 @@ var countTime = function(fn){
 		})
 	})
 }
+
+
+var countClickTable = function(){
+	return Object.keys(valid_arr).map(function(a,b){
+		var name = a
+		console.log(name)
+        var answer = valid_arr[a]
+        return Object.keys(answer).map(function(a,b){
+                return answer[a]
+         }).reduce(function(acc,a){
+            var spl = a.label.split("-")
+            if(spl.length >2){
+				if(acc.label == undefined){
+					acc.label = name
+				}
+                if(acc["col_"+spl[2]] == undefined){
+                     acc["col_"+spl[2]] = 0
+                     if(a.checked == true){
+                        acc["col_"+spl[2]] += 1
+                        return acc
+                     }else{
+                        return acc
+                     }
+                }else{
+                    if(a.checked){
+                        acc["col_"+spl[2]] += 1
+                        return acc
+                     }
+                    return acc
+                }
+            }else{
+                return "To nie jest matryca"
+            }
+         },{})
+    })
+}
+
+var timeStartEnd = function(fn){
+	return countTime(fn).map(function(a,b){
+        var question = a
+        var name = question[question.length-1].label.split('-')[0]
+        var end_time = question[question.length-1].start
+        return {label:name,start_end_time:end_time}
+    })
+}
+
+var averageTime = function(fn){
+	return countTime(fn).map(function(a,b){
+        var question = a,
+        name = question[question.length-1].label.split('-')[0],
+        acc = []
+        question.map(function(a,b){
+            if(b>0){
+                acc.push(a.beetwen)
+            }
+        })
+        var avg = Math.round(acc.reduce(function(sum, a) { return sum + a },0)/(acc.length||1))
+        return {label:name, avg_time:avg} 
+    });
+}
+
+var sequenceAnswer = function(fn){
+	return countTime(fn).map(function(a,b){
+        var question = a,
+        name = question[question.length-1].label.split('-')[0],
+        acc = []
+        question.map(function(a,b){           
+                acc.push(a.label)
+        })
+        return {label:name, sequence:acc} 
+    });
+}
+        return Object.keys(answer).map(function(a,b){
+            return answer[a]
+        }).filter(function(a,b){
+            return a.checked != null && a.checked != false
+            })
+    })
+}
+
+var countTime = function(fn){
+	return fn().map(function(a,b){
+		var question = a,
+		time = []
+		question.map(function(a,b){
+			time.push([a.label,a.click_time,a.checked])
+		})
+		var sort_time = time.sort(function(a, b) {
+	    return a[1] - b[1];
+		});
+
+		return sort_time.map(function(a,b){
+			var el = {}
+			el.label = sort_time[b][0]
+			el.timestamp = sort_time[b][1]
+			el.checked = sort_time[b][2]
+			if(b>0){
+	            el.beetwen = (sort_time[b][1] - sort_time[(b-1)][1])
+	            el.start = (sort_time[b][1] - sort_time[0][1])
+	        }else{
+	            el.beetwen = "start"
+	            el.start = "start"
+			}
+			return el
+		})
+	})
+}
